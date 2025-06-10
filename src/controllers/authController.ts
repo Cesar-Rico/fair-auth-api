8f101bba8314b9db6b62dc0a6e90d87a78293932
Luis Felipe Torres Granda <luisfelipetg96@Luis-Felipes-Mac-mini-2.local>
2025-06-09 19:51:24 -0500

    se corrige el auth controller

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -4,20 +4,25 @@ import { User } from '../models/user';
 // TODO: Cambiar a POST para prod
 
 let users: User[] = [];
-export const createUser = (req: Request, res: Response) => {
-    const user = new User(req.body);
-    users.push(user);
-    res.json({"message": "Usuario creado exitosamente"});
+export const createUser = async(req: Request, res: Response) => {
+    try{
+        const user = await User.create(req.body);
+        users.push(user);
+        res.json({"message": "Usuario creado exitosamente"});
+    } catch (error) {
+        console.error("Error al crear el usuario:", error);
+        res.status(400).json({"message": (error as Error).message || "Error al crear el usuario"});
+    }
 }
 
-export const loginUser = (req: Request, res: Response) => {
+export const loginUser = async (req: Request, res: Response) => {
     const username = req.body.username;
     const password = req.body.password;
 
     let userFind: User | undefined = users.find(objUser => objUser.user === username);
    
     if(userFind){
-        const match: boolean = userFind.verifyPassword(password);
+        const match: boolean = await userFind.verifyPassword(password);
         res.json({"validado": match, "message": match ? "Usuario validado correctamente": "Contraseña no valida"});
     }else{
         res.json({"validado": false, "message": "No se encontro usuario en base de datos"});
